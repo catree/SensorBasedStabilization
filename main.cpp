8bc56af6b6afa96ff007c443248bb1195b240e0e
tonyke1993 <tonyke1993@gmail.com>
2015-01-05 11:14:30 +0800

    correct an error in Matrix4x3.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,18 @@
 #include"VideoStabilization.h"
 #include "Quaternion.h"
 #include "test.h"
+#include "Matrix4x3.h"
 
 using namespace cv;
 using namespace std;
 
 int main() {
     //const string videoName = "20150104_105709";
-    const string videoName = "20150104_145300";
+    const string videoName = "20150104_145554";
     VideoStabilization a(videoName);
-    a.smooth();
+    /*a.smooth();
     a.show();
-    a.output();
+    a.output();*/
 
 //    test(videoName);
 
@@ -31,13 +32,18 @@ int main() {
 //    a.rotate(b1, b1, a.rotationMat(EulerAngles(-0.1, 0, 0)));
     imshow("ha", b1);
     waitKey(0);*/
-    /*Quaternion q, q1, q2, q3, q4,q5;
-    q.setToRotateInertialToObject(EulerAngles(0.5, 0.2, 0));
+/*    Quaternion q, q1, q2, q3, q4, q5;
+    Matrix4x3 m;
+    EulerAngles e;
+    q.setToRotateInertialToObject(EulerAngles(1, 0.5, 0));
+    e.fromInertialToObjectQuaternion(q);
+    q.w = 0.848, q.x = 0.237, q.y = 0.474, q.z = 0;
+    m.fromQuaternion(q);
     q1.setToRotateInertialToObject(EulerAngles(-0.5, -0.2, 0));
     q2.setToRotateInertialToObject(EulerAngles(-0.5, 0, 0));
     q3.setToRotateInertialToObject(EulerAngles(0, -0.2, 0));
-    q4=q*q1;
-    q5=q*q3*q2;*/
+    q4 = q * q1;
+    q5 = q * q3 * q2;*/
     return 0;
 }
 
